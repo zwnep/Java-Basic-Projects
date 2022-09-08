b6a5104d320d1e7ee5e2339eb23f5b4b62c76701
zeynep demirtaş <71128703+zwnep@users.noreply.github.com>
2022-09-08 12:52:08 +0300

    Add files via upload

---
+++ Not_ortalaması.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Create the variables.
+        int math, physics, chem, turkish, hist, music;
+
+        //Define the Scanner class.
+        Scanner input = new Scanner(System.in);
+
+        //Get user values.
+        System.out.print("Enter your math grade: ");
+        math = input.nextInt();
+
+        System.out.print("Enter your physics note: ");
+        physics = input.nextInt();
+
+        System.out.print("Enter your chemistry note: ");
+        chem = input.nextInt();
+
+        System.out.print("Enter your Turkish note: ");
+        turkish = input.nextInt();
+
+        System.out.print("Enter your history note: ");
+        hist = input.nextInt();
+
+        System.out.print("Enter your music note: ");
+        music = input.nextInt();
+
+        double average = (physics + chem + math + turkish + hist + music)/6;
+        System.out.println("Average of your course grades: " + average);
+
+        //Without using if-else, if the user's average is greater than 60, it should display "Passed the Class" and if it is less than "Failed in Class".
+        String passOrFail = (average > 60) ? "Passed the Class" : "Failed in Class";
+        System.out.print("Average: " + average + "\nPass Status: " + passOrFail);
+
+
+    }
+}
