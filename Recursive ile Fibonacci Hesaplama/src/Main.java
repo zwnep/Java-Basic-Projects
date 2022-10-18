486d032a47f4d71e49aab7dcf3145fbd939386ec
zeynep demirtaş <71128703+zwnep@users.noreply.github.com>
2022-10-18 21:15:06 +0300

    Add files via upload

---
+++ Recursive ile Fibonacci Hesaplama/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(fib(6));
+    }
+
+    static int fib(int n){
+        if(n==1||n==2) return 1;
+        return fib(n-1) + fib(n-2);
+
+    }
+}
