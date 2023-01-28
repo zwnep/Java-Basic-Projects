30fbc5561c46b03140297a49d1ad8384230e7eb4
zeynep demirtaş <71128703+zwnep@users.noreply.github.com>
2023-01-29 00:44:45 +0300

    Add files via upload

---
+++ Çin Zodyağı Hesaplama/src/Main.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Doğum Yılınızı Girin: ");
+        int yas = input.nextInt();
+        int zodyak = yas % 12;
+
+        switch (zodyak){
+            case 0:
+                System.out.print("Maymun");
+                break;
+            case 1:
+                System.out.print("Horoz");
+                break;
+            case 2:
+                System.out.print("Köpek");
+                break;
+            case 3:
+                System.out.print("Domuz");
+                break;
+            case 4:
+                System.out.print("Fare");
+                break;
+            case 5:
+                System.out.print("Öküz");
+                break;
+            case 6:
+                System.out.print("Kaplan");
+                break;
+            case 7:
+                System.out.print("Tavşan");
+                break;
+            case 8:
+                System.out.print("Ejerha");
+                break;
+            case 9:
+                System.out.print("Yılan");
+                break;
+            case 10:
+                System.out.print("At");
+                break;
+            case 11:
+                System.out.print("Koyun");
+                break;
+        }
+
+    }
+}
